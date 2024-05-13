a01682c17a2d2fd147cb71713cb835f752264f13
msa46 <mohamad.sanaye462@gmail.com>
2024-05-13 19:21:41 +0330

    Add github pages capabilities

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const options = {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-tech-blog-ten.vercel.app/',
+  site: 'https://msa46.github.io',
   markdown: {
     syntaxHighlight: false,
     // Disable syntax built-in syntax hightlighting from astro
